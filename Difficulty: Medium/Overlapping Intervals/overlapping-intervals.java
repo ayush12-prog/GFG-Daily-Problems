98aba9a3b395dcd83bb33aa025f63db0cae829be
ayush12-prog <ayushkumar991890@gmail.com>
2025-12-17 21:15:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        ArrayList<int[]> res = new ArrayList<>();
+
+        int start = arr[0][0];
+        int end = arr[0][1];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] <= end) {
+                end = Math.max(end, arr[i][1]);
+            } else {
+                res.add(new int[]{start, end});
+                start = arr[i][0];
+                end = arr[i][1];
+            }
+        }
+
+        res.add(new int[]{start, end});
+        return res;
+    }
+}
