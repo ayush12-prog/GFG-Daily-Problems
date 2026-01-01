3036d8210b5bb8c860d15b5d3aac548c2a4eb7a9
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-01 21:25:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node intersectPoint(Node head1, Node head2) {
+        Node ptr1 = head1;
+        Node ptr2 = head2;
+
+        while (ptr1 != ptr2) {
+            ptr1 = (ptr1 == null) ? head2 : ptr1.next;
+            ptr2 = (ptr2 == null) ? head1 : ptr2.next;
+        }
+
+        return ptr1;
+    }
+}
