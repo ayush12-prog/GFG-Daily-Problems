6e37fb23f3a3331059d29b41cf9769e5a73da4f7
Ayush Kumar <ayushkumar991890@gmail.com>
2026-01-25 17:32:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Number of Valid Parentheses/number-of-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findWays(int n) {
+        if (n % 2 != 0) {
+            return 0;
+        }
+        
+        int k = n / 2;
+        long[] dp = new long[k + 1];
+        dp[0] = 1;
+        
+        for (int i = 1; i <= k; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - 1 - j];
+            }
+        }
+        
+        return (int) dp[k];
+    }
+}
