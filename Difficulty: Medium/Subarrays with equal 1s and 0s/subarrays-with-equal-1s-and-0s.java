7063a415bbf84771cd2242938c639dbfb37dd360
Ayush Kumar <ayushkumar991890@gmail.com>
2026-02-25 19:11:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarrays with equal 1s and 0s/subarrays-with-equal-1s-and-0s.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countSubarray(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        int sum = 0;
+        int count = 0;
+        
+        for (int num : arr) {
+            sum += (num == 0) ? -1 : 1;
+            
+            if (map.containsKey(sum)) {
+                count += map.get(sum);
+            }
+            
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
