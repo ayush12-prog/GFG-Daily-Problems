f7ccec7fa6512fd96ea6597c8c813723e216b3eb
Ayush Kumar <ayushkumar991890@gmail.com>
2026-04-10 21:12:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.java
+++ Difficulty: Medium/Sorted subsequence of size 3/sorted-subsequence-of-size-3.java
@@ -1,19 +1,23 @@
 class Solution {
-    public ArrayList<Integer> find3Numbers(int[] arr) {
-        ArrayList<Integer> result = new ArrayList<>();
+    public List<Integer> find3Numbers(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+        
+        if (arr == null || arr.length < 3) {
+            return result;
+        }
         
         int first = Integer.MAX_VALUE;
         int second = Integer.MAX_VALUE;
-        int seqFirst = Integer.MAX_VALUE;
+        int firstForSecond = Integer.MAX_VALUE;
         
         for (int num : arr) {
             if (num <= first) {
                 first = num;
             } else if (num <= second) {
                 second = num;
-                seqFirst = first;
+                firstForSecond = first; 
             } else {
-                result.add(seqFirst);
+                result.add(firstForSecond);
                 result.add(second);
                 result.add(num);
                 return result;
