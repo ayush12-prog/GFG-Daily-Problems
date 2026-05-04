5dca280c6338c68ee7150480a62c7815aecb5901
Ayush Kumar <ayushkumar991890@gmail.com>
2026-05-04 20:48:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome Binary/palindrome-binary.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBinaryPalindrome(int n) {
+        String s = Integer.toBinaryString(n);
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
